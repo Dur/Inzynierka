823993b37704c0141d8456814e32040fc5d9a5f9
Dur <dur@dur-virtual.(none)>
2013-04-06 11:17:44 +0200

    uspojnianie danych

--- ServerSide/handlers/makeCoherent_wsh.py
+++ ServerSide/handlers/makeCoherent_wsh.py
@@ -29,12 +29,14 @@ def web_socket_transfer_data(request):
 		db = MySQLdb.connect(dbParamsDict["HOST"], login, password, dbParamsDict["DATABASE"])
 		cursor = db.cursor()
 		logging.info(NAME + "polaczenie z baza nawiazane")
-		cursor.execute("select * from versions where id >" + clientVersion + " order by id")
+		cursor.execute("select * from versions where id > " + clientVersion + " order by id")
+		logging.info(NAME + "komenda wyslana do bazy")
 		for version, command in cursor.fetchall():
 			socket.send_message(version)
 			socket.send_message(command)
 			logging.info(NAME + "Wyslano " + version + " " + command)
 		socket.send_message(END)
+		logging.info(NAME + "wyslano wiadomosc konczaca")
 	except MySQLdb.Error, e:
 		print("%d %s" % (e.args[0], e.args[1]))
 	except Exception, ee:
