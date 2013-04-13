e3abffb38ea933512b6d3eff1a3713e2a96e3538
Dur <dur@dur-virtual.(none)>
2013-04-13 14:16:01 +0200

    Poprawki w logerach - zmiana error na info + poprawione komentarze na Polskie

--- ServerSide/handlers/newHost_wsh.py
+++ ServerSide/handlers/newHost_wsh.py
@@ -1,6 +1,7 @@
 import Queue
 import logging
 import time
+from connections.Connection import Connection
 from connections.ListenSocket import ListenSocket
 from connections.PingConnection import PingConnection
 from utils.FileProcessors import FileProcessor
@@ -47,7 +48,7 @@ def web_socket_transfer_data(request):
 			if addresses[key] == 'F':
 				logging.info(NAME+ "znalazl dopasowanie")
 				logging.info(NAME+ "proba nawiazania polaczenia z nowododanym serwerem")
-				paramsDictionary["CONNECTION"] = PingConnection(paramsDictionary["HOME_PATH"]+"ServerSide/config/ping_config.conf")
+				paramsDictionary["CONNECTION"] = Connection(paramsDictionary["HOME_PATH"]+"ServerSide/config/ping_config.conf")
 				paramsDictionary["CONNECTION"].connect(remoteAddress, 80, RESOURCE)
 				paramsDictionary["SOCKET"].send_message(PONG)
 				logging.info(NAME+ "Server odpowiedzial PONG do " + paramsDictionary["CLIENT_ADDRESS"])
