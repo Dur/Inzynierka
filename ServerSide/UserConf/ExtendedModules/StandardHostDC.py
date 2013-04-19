b185b1bd7f87760d1193be21714f3708e1694139
Dur <dur@dur-virtual.(none)>
2013-04-19 12:45:20 +0200

    szybki commit

--- ServerSide/UserConf/ExtendedModules/StandardHostDC.py
+++ ServerSide/UserConf/ExtendedModules/StandardHostDC.py
@@ -11,11 +11,19 @@ def execute(paramsDictionary, message):
 	if( paramsDictionary["CONNECTION_MODE"] == True ):
 		paramsDictionary["CONNECTION"]._socket.close()
 	logging.info(NAME+ "Proba zapisu do pliku adresowego")
-	file.lockFile()
-	addresses = file.readFile()
-	addresses[paramsDictionary["CLIENT_ADDRESS"]] = 'F'
-	file.writeToFile(addresses)
-	file.unlockFile()
-	logging.info(NAME+ "Plik adresowy zaktualizowany")
-	if file.lock.is_locked:
-		file.unlockFile()
+	writeOk = False
+	while writeOk != True:
+		try:
+			file.lockFile()
+			logging.info(NAME + "Plik adresowy zablokowany")
+			writeOk = True
+			addresses = file.readFile()
+			addresses[paramsDictionary["CLIENT_ADDRESS"]] = 'F'
+			file.writeToFile(addresses)
+			file.unlockFile()
+			logging.info(NAME+ "Plik adresowy zaktualizowany")
+			if file.lock.is_locked:
+				file.unlockFile()
+		except Exception, e:
+			logging.error(NAME + "Nie mozna zablokowac pliku adresowego, czekanie bedzie wznowione")
+
